4dcc65f5c5e2a369c539d5f38f97e06b074269a3
Bus42 <gbrewton1980@gmail.com>
2018-06-23 13:52:03 -0500

    Adds logos

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,10 +5,10 @@ export class Footer extends Component {
     render() {
         return (
             <div id="footer-wrapper">
-                <a href="http://www.facebook.com/gwbrewton" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-square"></i></a>
-                <a href="https://twitter.com/Bus42a" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
-                <a href="https://www.linkedin.com/in/gregbrewton" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
-                <a href="https://github.com/Bus42" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                <a href="http://www.facebook.com/gwbrewton" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square"></i></a>
+                <a href="https://twitter.com/Bus42a" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/gregbrewton" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                <a href="https://github.com/Bus42" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
             </div>
         );
     }
---
+++ src/components/logo-bin/LogoBin.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './LogoBin.css';
+import '../../assets/bootstrap.png';
+import babelLogo from '../../assets/babel.png';
+import bootstrapLogo from '../../assets/bootstrap.png';
+import cssLogo from '../../assets/css3.svg';
+import gulpLogo from '../../assets/gulp.svg';
+import htmlLogo from '../../assets/html5.png';
+import jqueryLogo from '../../assets/jquery.png';
+import lessLogo from '../../assets/less.png';
+import mongodbLogo from '../../assets/mongodb.png';
+import nodejsLogo from '../../assets/nodejs.png';
+import reactLogo from '../../assets/react.png';
+import sassLogo from '../../assets/sass.png';
+
+export class LogoBin extends Component {
+    render() {
+
+        return (
+            <div id="logoBin-wrapper" >
+                <img className="logo" src={babelLogo} alt='Babel logo' />
+                <img className="logo" src={bootstrapLogo} alt='Bootstrap logo' />
+                <img className="logo" src={cssLogo} alt='CSS logo' />
+                <img className="logo" src={gulpLogo} alt='Gulp logo' />
+                <img className="logo" src={htmlLogo} alt='HTML5 logo' />
+                <img className="logo" src={jqueryLogo} alt='jQuery logo' />
+                <img className="logo" src={lessLogo} alt='Less logo' />
+                <img className="logo" src={mongodbLogo} alt='Mongo DB logo' />
+                <img className="logo" src={nodejsLogo} alt='Node JS logo' />
+                <img className="logo" src={reactLogo} alt='React JS logo' />
+                <img className="logo" src={sassLogo} alt='Sass logo' />
+
+            </div>
+        )
+    }
+}
