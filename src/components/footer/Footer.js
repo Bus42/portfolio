8f847b66ad9e9cbf3e392fea8df61757fb12018b
Bus42 <gbrewton1980@gmail.com>
2018-06-23 12:43:01 -0500

    Adds fontawesome cdn and footer content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,10 @@ export class Footer extends Component {
     render() {
         return (
             <div id="footer-wrapper">
-                <h2>Footer component works!</h2>
+                <a href="http://www.facebook.com/gwbrewton" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-square"></i></a>
+                <a href="https://twitter.com/Bus42a" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/gregbrewton" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/Bus42" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
             </div>
         );
     }
