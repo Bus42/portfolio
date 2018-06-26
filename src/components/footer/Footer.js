01d4f6303929fe684b24769e3c933966352ee427
Bus42 <gbrewton1980@gmail.com>
2018-06-26 16:26:09 -0500

    Creates prod build and adds email link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,6 +9,7 @@ export class Footer extends Component {
                 <a className="contact-link" href="https://twitter.com/Bus42a" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
                 <a className="contact-link" href="https://www.linkedin.com/in/gregbrewton" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
                 <a className="contact-link" href="https://github.com/Bus42" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                <a className="contact-link" href="mailto:gbrewton1980@gmail.com?subject=Portfolio" ><i class="far fa-envelope"></i></a>
             </div>
         );
     }
