8bff78b75619944b82cd273d0015ca31a960980e
Bus42 <gbrewton1980@gmail.com>
2018-10-23 21:17:16 -0600

    changes if to ternary in panelbody

--- src/components/panelBodyContent/PanelBodyContent.js
+++ src/components/panelBodyContent/PanelBodyContent.js
@@ -1,29 +1,23 @@
 import React from "react";
-import { Image } from 'react-bootstrap';
+import { Image } from "react-bootstrap";
 
-const PanelBodyContent = ({app}) => {
-  if (window.screen.height > 1000 && window.screen.width > 1000 && app.iframe) {
-    return (
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe
-          className="embed-responsive-item"
-          allowFullScreen
-          title={app.title}
-          src={app.href}
-        />
-      </div>
-    );
-  } else {
-      return (
-          <a href={app.href} target={app.title}>
-                        <Image
-                          rounded
-                          style={{ maxWidth: "90%" }}
-                          src={app.image}
-                        />
-                      </a>
-      )
-  }
+const PanelBodyContent = ({ app }) => {
+  return window.screen.height > 1000 &&
+    window.screen.width > 1000 &&
+    app.iframe ? (
+    <div className="embed-responsive embed-responsive-16by9">
+      <iframe
+        className="embed-responsive-item"
+        allowFullScreen
+        title={app.title}
+        src={app.href}
+      />
+    </div>
+  ) : (
+    <a href={app.href} target={app.title}>
+      <Image rounded style={{ maxWidth: "90%" }} src={app.image} />
+    </a>
+  );
 };
 
 export default PanelBodyContent;
